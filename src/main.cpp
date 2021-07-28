76f94eb1924969435ebe0d979764b6d4977d273d
JoelFrancisco <joelfranciscofilho@gmail.com>
2021-07-28 17:13:36 -0300

    first

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,10 @@ void setup() {
   // put your setup code here, to run once:
   //modi on]
   pinMode(13,OUTPUT);
+  pinMode(12, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  // TESTE
 }
